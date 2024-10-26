f2ff172d962b72195dad11374a3a0bb09f3905d2
TeddyMedjio <nganmedjiojeanclaude@gmail.com>
2024-10-26 13:54:31 +0100

    detail de produit

--- components/ProductListItem.tsx
+++ components/ProductListItem.tsx
@@ -2,10 +2,7 @@
 import { Card } from "@/components/ui/card";
 import { Image } from "@/components/ui/image";
 import { Heading } from "@/components/ui/heading";
-import { VStack } from "@/components/ui/vstack";
-import { Box } from "@/components/ui/box";
 import { Text } from "@/components/ui/text";
-import { Button, ButtonText } from "@/components/ui/button";
 import { Link } from "expo-router";
 import { Pressable } from "react-native";
 
@@ -28,27 +25,9 @@ export default function ProductListItem({product}:any){
         <Text className="text-sm font-normal mb-2 text-typography-700">
          { product.name}
         </Text>
-        <VStack className="mb-6">
           <Heading size="md" className="mb-4">
             ${product.price}
           </Heading>
-          <Text size="sm">
-            {product.description}
-          </Text>
-        </VStack>
-        <Box className="flex-col sm:flex-row">
-          <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
-            <ButtonText size="sm">Add to cart</ButtonText>
-          </Button>
-          <Button
-            variant="outline"
-            className="px-4 py-2 border-outline-300 sm:flex-1"
-            >
-            <ButtonText size="sm" className="text-typography-600">
-              Wishlist
-            </ButtonText>
-          </Button>
-        </Box>
       </Card>
       </Pressable> 
       </Link>
